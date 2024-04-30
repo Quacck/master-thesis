2e27536e8669d97c2e71071ee21b95ff1c2c738c
Vincent <vincopi@googlemail.com>
2024-04-30 12:27:33 +0200

    Add super simple round robin scheduling

--- implementation/scheduling_simulation.py
+++ implementation/scheduling_simulation.py
@@ -10,11 +10,12 @@ from dataclasses import dataclass
 from typing import List, Iterator, Type
 
 class Job:
-    def __init__(self, env: simpy.Environment, id, deadline: simpy.core.SimTime, runtime: simpy.core.SimTime):
+    def __init__(self, env: simpy.Environment, id, deadline: simpy.core.SimTime, runtime: simpy.core.SimTime, overhead: int):
         all_jobs.append(self)
         self.id = id
         self.deadline = deadline
         self.runtime = runtime
+        self.overhead = overhead
         self.env = env
         self.log = {
             'submit_time': env.now,
@@ -25,7 +26,7 @@ class Job:
     def do_something(self):
         self.log.update({'start_time': self.env.now})
         yield self.env.timeout(self.runtime)
-        self.log.update({'end_time': self.env.now})
+        self.log.update({'end_time': self.env.now})        
 
 class ComputeNode:
     def __init__(self, env: simpy.Environment, id, cost_shutdown, duration_shutdown, cost_boot, duration_boot, scheduler):
@@ -61,8 +62,40 @@ class ComputeNode:
             yield self.env.process(job.do_something())
             end_time = self.env.now
             log.get('compute_node_events').append({'type': 'execute_job', 'id': 'Node' + str(self.id), 'start': start_time, 'end': end_time, 'time': end_time - start_time, 'job': job})
+            self.ressource.release(request) 
             self.set_state('READY')
             self.scheduler.schedule_jobs()
+
+    def stop_current_job(self):
+        if (len(self.ressource.users) == 0):
+            raise Exception("Trying to stop job while no job is currently being run on Node " + self.id)
+
+    def execute_job_for_duration(self, job: Job, duration: int):
+        with self.ressource.request() as request:
+            yield request
+            self.set_state('WORKING')
+            start_time = self.env.now
+
+            adjusted_runtime = duration if job.runtime > duration else job.runtime
+
+            job_with_duration = Job(self.env, job.id, job.deadline, adjusted_runtime, job.overhead)
+            yield self.env.process(job_with_duration.do_something())
+            end_time = self.env.now
+            log.get('compute_node_events').append({'type': 'execute_job', 'id': 'Node' + str(self.id), 'start': start_time, 'end': end_time, 'time': end_time - start_time, 'job': job})
+
+            # if the job is not yet complete, we suppose that there needs to be some amount of overhead such as saving the current state.
+
+            remaining_work = Job(self.env, job.id, job.deadline, job.runtime - duration, job.overhead)
+            if (remaining_work.runtime >= 0):
+                log.get('compute_node_events').append({'type': 'overhead', 'id': 'Node' + str(self.id), 'start': self.env.now, 'end': self.env.now + job.overhead, 'time': job.overhead, 'job': job})
+                yield self.env.timeout(job.overhead)
+                self.set_state('READY')
+                self.scheduler.submit_job(remaining_work)
+
+            self.ressource.release(request) 
+            self.set_state('READY')
+            self.scheduler.schedule_jobs()
+
     
     def get_current_power(self) -> int:
         match self.state:
@@ -120,108 +153,26 @@ class FiFoScheduler(BaseScheduler):
             first_available_compute_node.state = 'STARTING'
             self.env.process(first_available_compute_node.execute_job(job))
 
-class SimpleGreenScheduler(BaseScheduler):
+
+    def cancel_job(self, compute_node: ComputeNode):
+        pass
+        
+
+class PreemptiveRoundRobinScheduler(BaseScheduler):
     """
-    Similar to the scheduler suggested by the Paper "GreenSlot: Scheduling Energy Consumption in Green Datacenters",
-    this scheduler executes jobs preferably during times of high co2-efficiency. Only if deadlines cannot be met otherwise,
-    work jobs for cheap brown energy. For simpleness' sake, we don't model the cost of energy here, so executing jobs on high brown energy 
-    will be done whenever.
-
-    In the original paper, green energy is supplied via a solar array. In our model, we check for intervals where co2 production is
-    low-ish.
+    Schedule incoming jobs only for a limited amount of time.
     """
-
-    def add_row_to_scheduling_matrix(self, timestamp: int):
-        predicted_co2 = carbon_predictor.get_carbon_intensity_at_timestamp(timestamp)
-        self.scheduling_matrix.loc[timestamp] = [timestamp, predicted_co2] + ['READY' for i in range(0, config.number_of_compute_nodes)]
-
-    def __init__(self, env: simpy.Environment):
-        super().__init__(env)
-
-        """
-        We keep our current state in a matrix
-
-        +-----------+----------+------------+------------+-----+
-        | Timestamp | gCo2/kWh | StateNode1 | StateNode2 | ... |
-        +-----------+----------+------------+------------+-----+
-        | T1        |      134 | READY      | READY      |     |
-        | T2        |      129 | Start Job1 | READY      |     |
-        | T2        |      135 | Job1       | READY      |     |
-        +-----------+----------+------------+------------+-----+
-
-        which contains the model on what we schedule. This will be a scrolling window over time
-        with past-timestamp-rows continually being discarded when possible.
-        """
-
-        columns = ['Timestamp', 'gCo2/kWh'] + ['StateNode' + str(index) for index in range(0, config.number_of_compute_nodes)]
-        self.scheduling_matrix = pandas.DataFrame(columns=columns)
-        self.scheduling_matrix.set_index('Timestamp')     
-        self.add_row_to_scheduling_matrix(env.now)  
 
     def schedule_jobs(self):
-        # Look at 10 minute scheduling windods where jobs may start
-        # update our prediction window if needed
-        last_timestamp = self.scheduling_matrix.iloc[len(self.scheduling_matrix) - 1]['Timestamp']
-
-        # schedule for 2 days ahead  
-        while (last_timestamp - self.env.now < (2 * 24 * 60 * 60)):
-            last_timestamp += 10 * 60
-            self.add_row_to_scheduling_matrix(last_timestamp)
-
-        # remove rows that are in the past
-        past_rows_mask = self.scheduling_matrix['Timestamp'] < self.env.now
-        self.scheduling_matrix.drop(self.scheduling_matrix[past_rows_mask].index, inplace=True)
-
-        # schedule the next jobs
         while len(self.queue) > 0:
             job = self.queue.pop()
             log.get('job_queue').append({'type': 'pop', 'time': self.env.now, 'length': len(self.queue)})
-            average_co2 = self.scheduling_matrix['gCo2/kWh'].mean()
-            # find timeslots that have below average co2 emissions
-            node_is_available = np.array([False] * config.number_of_compute_nodes)
-            start_of_timeslot = np.array([None] * config.number_of_compute_nodes)
-
-            found_slot = False
-            for index, row in self.scheduling_matrix.iterrows():
-                if (found_slot is True):
-                    break
-                for i in range(0, config.number_of_compute_nodes):
-                    # the slot starts here
-
-                    label_of_node = 'StateNode'+ str(i)
-                    state_of_node = row[label_of_node]
-                    if (start_of_timeslot[i] == None and state_of_node == 'READY' and row['gCo2/kWh']<average_co2):
-                        start_of_timeslot[i] = row['Timestamp'] 
-                    elif (start_of_timeslot[i] != None and state_of_node != 'READY' or row['gCo2/kWh']>average_co2):
-                        start_of_timeslot[i] = None
-
-                    if  (start_of_timeslot[i] is not None and (row['Timestamp'] - start_of_timeslot[i]) > job.runtime):
-                        # we can schedule the job here
-
-                        is_first = True
-
-                        for index, row_to_be_updated in self.scheduling_matrix.loc[start_of_timeslot[i] : row['Timestamp']].iterrows():
-                            if (is_first):
-                                is_first = False
-                                self.scheduling_matrix.at[index, label_of_node] = job
-                                continue
-                            self.scheduling_matrix.at[index, label_of_node] = 'WORKING'
-
-                        found_slot = True    
-                        break # out of searching for a timeslot
-            if (found_slot is False):
+            first_available_compute_node: ComputeNode = next((node for node in self.nodes if node.state == 'READY'), None)
+            if (first_available_compute_node is None):
                 self.queue.append(job)
-
-        # we now planned on when to execute all jobs, now we need to actually start them too
-        schedule_to_be_executed = self.scheduling_matrix.iloc[0]
-        if (schedule_to_be_executed['Timestamp'] > self.env.now):
-            return
-
-        for i in range(0, config.number_of_compute_nodes):
-            value_of_plan = schedule_to_be_executed['StateNode'+ str(i)] 
-            if (isinstance(value_of_plan, Job)):
-                self.env.schedule()
-                self.env.process(self.nodes[i].execute_job(value_of_plan))
+                break
+            first_available_compute_node.state = 'STARTING'
+            self.env.process(first_available_compute_node.execute_job_for_duration(job, config.round_robin_quantum_length))
 
 @dataclass
 class SimulationConfig:
@@ -234,6 +185,8 @@ class SimulationConfig:
     submit_interval_generator_scale: int = 600
     job_length_generator_mean: int = 3600
     job_length_generator_scale: int = 1800
+
+    round_robin_quantum_length: int = 360
 
     idle_power_hour: float = 5
     working_power_per_hour: float = 10
@@ -256,32 +209,6 @@ class CarbonEmissionPredictor:
     # We run a linear interpolation between those points
     def get_carbon_intensity_at_timestamp(self, timestamp: int) -> int:
         return np.interp(x = timestamp, xp=self.carbon_data['timestamp'], fp=self.carbon_data['Carbon Intensity gCO₂eq/kWh (direct)'])
-
-class Scheduler:
-    def __init__(self, env: simpy.Environment):
-        self.env = env
-        self.nodes: List[ComputeNode] = [ComputeNode(env, i, 1,2,3,4, self) for i in range(config.number_of_compute_nodes)]
-
-        self.queue: List[Job] = []
-        self.env = env
-
-    def submit_job(self, job: Job):
-        self.queue.append(job)
-        log.get('job_queue').append({'type': 'append', 'time': self.env.now, 'length': len(self.queue)})
-
-        self.schedule_jobs()
-
-    def schedule_jobs(self):
-        while len(self.queue) > 0:
-            job = self.queue.pop()
-            log.get('job_queue').append({'type': 'pop', 'time': self.env.now, 'length': len(self.queue)})
-            first_available_compute_node: ComputeNode = next((node for node in self.nodes if node.state == 'READY'), None)
-            if (first_available_compute_node is None):
-                self.queue.append(job)
-                break
-            first_available_compute_node.state = 'STARTING'
-            self.env.process(first_available_compute_node.execute_job(job))
-
 
 class JobSubmitter:
     def __init__(self, env: simpy.Environment, scheduler: BaseScheduler):
@@ -321,7 +248,7 @@ class YamlJobSubmitter(JobSubmitter):
         with open('workloads/two_jobs.yml', 'r') as file:
             parsed = yaml.safe_load(file)
             for job in parsed:
-                jobs.append({'start': int(job['start']), 'length': int(job['length']), 'deadline': int(job['deadline'])})
+                jobs.append({'start': int(job['start']), 'length': int(job['length']), 'deadline': int(job['deadline']), 'overhead': int(job['overhead'])})
         jobs_sorted_by_start = sorted(jobs, key=lambda x: x['start'])
 
         yield self.env.timeout(jobs_sorted_by_start[0]['start'])
@@ -329,7 +256,8 @@ class YamlJobSubmitter(JobSubmitter):
             job_config = jobs_sorted_by_start[i]
             runtime = job_config['length']
             deadline = runtime + job_config['deadline']
-            job = Job(self.env, id, deadline, runtime)
+            overhead = job_config['overhead']
+            job = Job(self.env, i, deadline, runtime, overhead)
             self.scheduler.submit_job(job)
 
             if (i + 1 == len(jobs_sorted_by_start)):
